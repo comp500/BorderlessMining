34bf9ed91c5f242023330c05e1e62496da41f67c
comp500 <comp500@users.noreply.github.com>
2020-01-11 03:45:11 +0000

    Rename all the things

--- src/main/java/link/infra/borderlessmining/BorderlessMining.java
+++ src/main/java/link/infra/borderlessmining/BorderlessMining.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package link.infra.borderlessmining;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class BorderlessMining implements ClientModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
--- src/main/java/link/infra/borderlessmining/mixin/ExampleMixin.java
+++ src/main/java/link/infra/borderlessmining/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example.mixin;
+package link.infra.borderlessmining.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
