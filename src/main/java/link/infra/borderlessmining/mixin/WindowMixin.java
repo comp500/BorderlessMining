555a13a6a78e3699ea13ef1977a0e8c076c5c1db
comp500 <comp500@users.noreply.github.com>
2020-01-13 17:04:13 +0000

    Fix obfuscated crash using intrinsic proxies

--- src/main/java/link/infra/borderlessmining/mixin/WindowMixin.java
+++ src/main/java/link/infra/borderlessmining/mixin/WindowMixin.java
@@ -11,10 +11,7 @@ import net.minecraft.client.WindowSettings;
 import net.minecraft.client.util.MonitorTracker;
 import net.minecraft.client.util.Window;
 import org.lwjgl.glfw.GLFW;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,6 +19,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Window.class)
 public abstract class WindowMixin implements WindowBoundsGetter {
+	/* Implementation of WindowBoundsGetter with Intrinsic Proxies */
+	@Shadow public abstract int getX();
+	@Shadow public abstract int getY();
+	@Shadow public abstract int getWidth();
+	@Shadow public abstract int getHeight();
+	@Intrinsic(displace = true)
+	public int windowboundsgetter$getX() {
+		return getX();
+	}
+	@Intrinsic(displace = true)
+	public int windowboundsgetter$getY() {
+		return getY();
+	}
+	@Intrinsic(displace = true)
+	public int windowboundsgetter$getWidth() {
+		return getWidth();
+	}
+	@Intrinsic(displace = true)
+	public int windowboundsgetter$getHeight() {
+		return getHeight();
+	}
+
 	@Shadow
 	private boolean fullscreen;
 
