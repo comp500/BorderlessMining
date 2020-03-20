42e31dc847d99a70f3ece692ed5423b065b1e0aa
comp500 <comp500@users.noreply.github.com>
2020-03-20 01:57:59 +0000

    Add fix for MC-175437 (Fullscreen resolution isn't applied when exiting Video Settings with Esc)

---
+++ src/main/java/link/infra/borderlessmining/mixin/VideoModeFixMixin.java
@@ -0,0 +1,29 @@
+package link.infra.borderlessmining.mixin;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.VideoOptionsScreen;
+import net.minecraft.client.gui.screen.options.GameOptionsScreen;
+import net.minecraft.client.options.GameOptions;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * This Mixin fixes a bug in Keyboard that causes video mode changes to not be applied when pressing Esc.
+ * See: https://bugs.mojang.com/browse/MC-175437
+ */
+@Mixin(VideoOptionsScreen.class)
+public class VideoModeFixMixin extends GameOptionsScreen {
+	private VideoModeFixMixin(Screen parent, GameOptions gameOptions, Text title) {
+		super(parent, gameOptions, title);
+	}
+
+	@Inject(at = @At("HEAD"), method = "removed()V")
+	public void screenRemoved(CallbackInfo ci) {
+		if (this.minecraft != null) {
+			this.minecraft.getWindow().applyVideoMode();
+		}
+	}
+}
